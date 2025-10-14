cbbbdc78d0aa6af8f9b1eb20ac599823f14d205a
adhkar <adhkarpro@gmail.com>
2025-10-14 06:54:02 +0000

    Add bat.sh script

---
+++ bat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Source file
+SOURCE_FILE="./file.txt"
+
+# Destination folder
+DEST="."
+
+# Copy the file
+cp "$SOURCE" "$DEST"
+
+#print success message
+echo "File copied successfully!"
